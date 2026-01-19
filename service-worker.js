19b77aeea264059a0bf438979b5e32d56cf00529
ALEEF02 <thesmarty2002@gmail.com>
2026-01-18 16:13:39 -0800

    Update personal title

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'precache-v10';
-const RUNTIME = 'runtime-v9';
+const PRECACHE = 'precache-v11';
+const RUNTIME = 'runtime-v10';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
